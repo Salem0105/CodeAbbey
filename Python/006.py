3dc192c4429a8678dfdea7af69ba92040d75c60d
Salem <saulperez0105@gmail.com>
2025-09-23 06:09:56 -0500

    Reto 006, Corecciones del codigo Python, Solucion en Java agregada

--- Python/006.py
+++ Python/006.py
@@ -1,9 +1,9 @@
 # https://www.codeabbey.com/index/task_view/rounding
 
 # Funcion para determinar el redondeo
-def condition(num1, num2):
+def condition(num1, num2, iter):
     # Primero una division normal, para luego evaluar si es positivo o negativo
-    if (num1 / num2) % 1 > 0:
+    if (num1 / num2) > 0:
         # Si es positivo, evaluar si la parte decimal es mayor o igual a 0.5
         if (num1 / num2) % 1 >= 0.5:
             # Redondear hacia arriba
@@ -13,27 +13,29 @@ def condition(num1, num2):
             return num1 // num2
     else:
         # Si es negativo, evaluar si la parte decimal es menor o igual a -0.5
-        if (num1 / num2) % 1 <= -0.5:
+        if (num1 / num2) % 1 <= 0.5:
             # Redondear hacia abajo
-            return (num1 // num2) - 1
-        else:
             return num1 // num2
+        else:
+            # Quitar redondeo
+            return (num1 // num2) + 1
 
-def interation(total):    
+def itaration(total):
+    # Cadena para almacenar los resultados
+    chain = ""
     for i in range(total):
         # Recibir datos
         data = input()
         # Separar los datos
         separate = data.split(" ")
         # Llamar a la funcion y agregar el resultado a una variable reuilizable
-        add_data = condition(int(separate[0]), int(separate[1]))
+        add_data = condition(int(separate[0]), int(separate[1]), i)
         chain += f"{add_data} "
+
+    # Imprimir resultados
+    print(chain)
 
 # recibir primer dato
 receive = int(input())
-#Cadena para almacenar los resultados
-chain = ""
 # Llamar a la funcion
-interation(receive)
-# Imprimir resultados
-print(chain)
+itaration(receive)
